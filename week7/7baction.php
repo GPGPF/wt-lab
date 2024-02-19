e48f7cbfcb81e2c71ce73335bf8c4864a544ea29
pradeep_rl1 <gpgp070704@gmail.com>
2024-02-19 12:01:42 +0530

    add week7

---
+++ week7/7baction.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Action</title>
+</head>
+    <body bgcolor= <? $var=$_POST['co'] ; echo $var;?> >
+        <p style="font-size:<? $var1= $_POST['fsa']; echo $var1;?>px;color:green;" >
+        <? echo $_POST['uname'] ?></p>
+    </body>
+</html>
