8599a605fcaa46868653e50220ae57bfcdba45e2
rllab <gpgp070704@gmail.com>
2024-03-04 12:41:06 +0530

    add test folder

---
+++ test/hello.php
@@ -0,0 +1,18 @@
+<?php
+// $makefoo=true;
+// $var="hello";
+//     if($makefoo){
+//         function foo(){
+//             echo "I don't exits until program execution reaches me.";
+//             echo $GLOBALS['var'];
+//         }
+//     }
+//     if($makefoo){
+//          foo();
+//     }
+
+function make($type="attack",$color){
+    echo $type." ".$color;
+}
+
+?>
