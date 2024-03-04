0f696eb3bf37e76c837b54ef438da60c104b7692
rllab <gpgp070704@gmail.com>
2024-03-04 12:52:01 +0530

    update upto no return function example in notebook

--- test/hello.php
+++ test/hello.php
@@ -11,8 +11,37 @@
 //          foo();
 //     }
 
-function make($type="attack",$color){
-    echo $type." ".$color;
-}
+// function make($type="attack",$color){
+//     echo $type." ".$color;
+// }
+// make("blue"); //Raises an error
+
+
+// Solution
+// function make($color,$type="attack"){
+//     echo $type." ".$color;
+// }
+// make("blue");
+
+
+// function addone(&$n){
+//     return ++ $n; //If there is no return by default null value is returned.
+//     ++$n; //unreachable code
+// }
+// function multiplyseq($n){
+//     return (addone($n) * $n); //If one operand on either side is null then result will be zero for any operation. 
+// }
+// echo multiplyseq(2);
+
+
+// function multipleret($arg1){
+//     $arr=array();
+//     $arr[]="A";
+//     $arr[]=$arg1;
+//     return $arr;
+// }
+// print_r(multipleret("bla bla bla"));
+
+
 
 ?>
